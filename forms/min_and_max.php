05007a4092926270dab088cabad97045f66506e5
Vlad <start2top@gmail.com>
2021-09-17 04:00:38 +0300

    renamed folder and added radio form handler

---
+++ forms/min_and_max.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Min and Max</title>
+</head>
+<body>
+    <form action="min_and_max.php" method="POST">
+        <input type="text" name="a"><br><br>
+        <input type="text" name="b"><br><br>
+        <input type="text" name="c"><br><br>
+        <input type="text" name="d"><br><br>
+        <input type="text" name="e"><br><br>
+        <input type="text" name="f"><br><br>
+        <input type="text" name="g"><br><br>
+        <input type="submit" name="submit" value="Отправить">
+    </form>
+<?php
+if (isset($_POST['submit'])) {
+    $a = intval($_POST['a']);
+    $b = intval($_POST['b']);
+    $c = intval($_POST['c']);
+    $d = intval($_POST['d']);
+    $e = intval($_POST['e']);
+    $f = intval($_POST['f']);
+    $g = intval($_POST['g']);
+}
+
+    $arr = array($a, $b, $c, $d, $e, $f, $g);
+
+    echo '<hr>';
+
+    echo "Вы отправили: A = $a, B = $b, C = $c, D = $d, E = $e, F = $f, G = $g" . '<br><br>';
+    
+    echo 'Минимальное значение:  ' . min($arr) . '<br><br>'; 
+    echo 'Максимальное значение:  ' . max($arr) . '<br><br>';
+    
+
+?>
+</body>
+</html>
---
+++ forms/radio_form.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Radio form</title>
+</head>
+<body>
+    <form action="radio_form.php" method="POST">
+            <p> Ваше имя: </p>
+        <input type="text" name="firstname" placeholder="Введите ваше имя">
+            <br><br>
+            <p>Ваш пол:</p>
+        <input type="radio" name="gender" value="male" checked="checked">Мужской
+        <input type="radio" name="gender" value="female">Женский
+                <br><br>
+        <input type="submit" value="отправить" name="submit">
+                <br><br><br>
+       
+    </form>
+<?php
+if(isset($_POST['submit']) && !empty($_POST['firstname'])) {
+    $name = $_POST['firstname'];
+    $gender = $_POST['gender'];
+
+
+   
+    if($gender == 'male') {
+        echo "Добро пожаловать, господин $name!";
+    } else {
+        echo "Добро пожаловать, госпожа $name!";
+    } 
+}
+?>
+</body>
+</html>
